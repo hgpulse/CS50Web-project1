e565407b05868e984132e4d33dedae0faba4f33a
Hugo <hugogaluppo@gmail.com>
2020-07-20 10:36:17 +0200

    Error Handling

---
+++ django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/huguinho/Documents/dev/CS50w/wiki/django-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ encyclopedia/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+from . import util
+import re
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+def entry(request, title):
+    entry_exist = util.get_entry(title)
+    titleMd = re.compile('#.*', re.MULTILINE)
+    if entry_exist:
+        titleHtml = titleMd.fullmatch(entry_exist)
+        return render (request, "encyclopedia/entry.html", {
+            "entry": entry_exist,
+            "titleHtml" : titleMd.findall(entry_exist)
+        })
+    else:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+    
+
+
