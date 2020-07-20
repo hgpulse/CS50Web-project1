e9088539381b1fc5b551dcab17855d65e8794734
Hugo <hugogaluppo@gmail.com>
2020-07-20 15:18:49 +0200

    fixed urls view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
-from . import util
 
+from . import util
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -10,12 +11,12 @@ def index(request):
     })
 def entry(request, title):
     entry_exist = util.get_entry(title)
-    titleMd = re.compile('#.*', re.MULTILINE)
+    
+    
     if entry_exist:
-        titleHtml = titleMd.fullmatch(entry_exist)
+  
         return render (request, "encyclopedia/entry.html", {
-            "entry": entry_exist,
-            "titleHtml" : titleMd.findall(entry_exist)
+            "entry": entry_exist
         })
     else:
         return HttpResponseNotFound('<h1>Page not found</h1>')
