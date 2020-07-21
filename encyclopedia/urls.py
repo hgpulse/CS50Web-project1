194a947a304b164b643f24c2341a073cefd870bd
Hugo <hugogaluppo@gmail.com>
2020-07-21 17:14:55 +0200

    search and create

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
-  
+    path("<str:title>", views.entry, name="entry"),
+    path('search/',views.search,name='search'),
+    path('create/',views.create ,name='create'),
+    path('validate/',views.validate ,name='validate')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 from . import util
 import markdown2
+from .forms import NameForm
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -22,20 +24,51 @@ def entry(request, title):
     else:
         return HttpResponseNotFound('<h1>Page not found</h1>')
 
-def get_entry(request):
-    search = request
-    # if this is a POST request we need to process the form data
-    if request.method == 'GET':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.GET)
-        return render(request, 'search_result.html')
 
 
-    # if a GET (or any other method) we'll create a blank form
+
+def search(request):
+    # get the string in the field
+    query = request.GET.get('q').lower()
+    print(query)
+    entry_exist = util.get_entry(query)
+    # check if the query is egual to an exeisting entry
+    if entry_exist:
+        return render (request, "encyclopedia/entry.html", {
+            "entry": entry_exist,
+            "title":query
+        })
     else:
-        form = NameForm()
+        #get the list of existing entries
+        entries = util.list_entries()
+        result = []
+        for entry in entries:
 
-    return render(request, 'search_result.html')
+            if query in entry.lower():
+                result.append(entry)
+        
+        return render(request, "encyclopedia/search.html", {
+        "search": result
+    })  
 
+def create(request):
+    return render(request, "encyclopedia/create.html")
 
-
+def validate(request):
+        
+        #lower case for comparaison
+        title = request.GET.get('title').lower()
+        entry = request.GET.get('entry').lower()
+        
+        #get the list of existing entries
+        entries = util.get_entry(title)
+        if entries:
+            return HttpResponseNotFound('<h1>Article already exist</h1>')
+    
+        util.save_entry(title.upper(), entry)
+        
+        #return the updated list
+        
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
